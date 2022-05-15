d23924a900c0647cb2a2650407c95b1d240b7d55
Nahuel Pascual <nahuel.pascual@mudafy.com>
2022-05-14 23:47:25 -0300

    add tests before refactoring

--- ruby/spec/contrato_spec.rb
+++ ruby/spec/contrato_spec.rb
@@ -1,41 +1,42 @@
 require_relative '../lib/contrato'
 
 describe Contrato do
-  pre_edad_block = false
-  let(:persona) { Persona.new }
 
-  before(:each) do
-    pre_edad_block = false
-  end
+  describe 'blocks PRE' do
+    pre_edad_executed = false
+    pre_another_method_executed = false
+    let(:persona) { Persona.new }
 
-  Persona = Class.new do
-    # invariant { !@edad.nil? && @edad > 0 }
-    # invariant { false }
-    invariant { true }
-
-    pre { pre_edad_block = true }
-    post { "".nil? }
-    def edad
-      puts 'la edad es ' + @edad.to_s
-      @edad
+    before(:each) do
+      pre_edad_executed = false
+      pre_another_method_executed = false
     end
 
-    def set_edad(n)
-      puts 'seteando edad en ' + n.to_s
-      @edad = n
+    Persona = Class.new do
+      pre { pre_edad_executed = true }
+      def edad
+        puts 'la edad es ' + @edad.to_s
+        @edad
+      end
+
+      pre { pre_another_method_executed = true }
+      def another_method
+        puts "ejemplo"
+      end
     end
 
-    pre { "".nil? }
-    post { "".nil? }
-
-    def another_method
-      puts "ejemplo"
+    it 'should execute only edad pre-block' do
+      persona.edad
+      expect(pre_edad_executed).to be true
+      expect(pre_another_method_executed).to be false
     end
-  end
 
-  it 'should execute edad pre-block' do
-    persona.edad
-    expect(pre_edad_block).to be true
+    it 'should execute edad and another_method pre-block' do
+      persona.edad
+      persona.another_method
+      expect(pre_edad_executed).to be true
+      expect(pre_another_method_executed).to be true
+    end
   end
 
 end
