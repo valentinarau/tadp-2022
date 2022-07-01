ad8930a8bae52dffd5ec6c7da36ea5d296727949
Matias Berenson <matias.berenson@gmail.com>
2022-07-01 10:58:49 -0300

    Fix tests

--- scala/src/test/scala/ProjectSpec.scala
+++ scala/src/test/scala/ProjectSpec.scala
@@ -89,7 +89,7 @@ class ProjectSpec extends AnyFreeSpec {
 
       "con Fuerza base de 40 y HP de 10, e inventario inicial vacío, al equiparse con un Casco Vikingo" - {
         "debería tener HP de 20" in {
-          val heroe = Heroe(None, baseStats.copy(fuerza = 40), Inventario())
+          val heroe = Heroe(None, baseStats.copy(fuerza = 40, hp = 10), Inventario())
           val heroeEquipado = heroe.copy(inventario = heroe.inventario.agregarItem(cascoVikingo, heroe))
           heroeEquipado.stats().hp shouldBe 20
         }
@@ -102,19 +102,19 @@ class ProjectSpec extends AnyFreeSpec {
     val modificadorIdempotente: Modificador = (s: Stats, _:Heroe) => s
     val heroe = Heroe(statsBase = Stats().initializeWith(10))
 
-    "Item equipable por heroes sin trabajo" - {
+    "Item equipable por heroes sin trabajo" in {
       val item = UnaMano(restriccion = SinTrabajo, modificadorIdempotente)
       item.puedeEquiparse(heroe) shouldBe(true)
       item.puedeEquiparse(heroe.especializarse(Mago)) shouldBe(false)
     }
 
-    "Item equipable por Guerreros" - {
+    "Item equipable por Guerreros" in {
       val item = UnaMano(restriccion = TrabajoIgualA(Guerrero), modificadorIdempotente)
       item.puedeEquiparse(heroe.especializarse(Guerrero)) shouldBe(true)
       item.puedeEquiparse(heroe.especializarse(Mago)) shouldBe(false)
     }
 
-    "Item equipable por Ladrones con Inteligencia > 30" - {
+    "Item equipable por Ladrones con Inteligencia > 30" in {
 
       val item = Talisman(restriccion = And(List(TrabajoIgualA(Ladron),StatMinimo(Inteligencia, 30))), modificadorIdempotente)
       item.puedeEquiparse(heroe.especializarse(Ladron)) shouldBe(false)
@@ -122,7 +122,7 @@ class ProjectSpec extends AnyFreeSpec {
       item.puedeEquiparse(Heroe(statsBase = Stats(inteligencia = 40)).especializarse(Ladron)) shouldBe(true)
     }
 
-    "Item equipable por Magos o Guerreros" - {
+    "Item equipable por Magos o Guerreros" in {
       val espadaDelDestino = DosManos(restriccion = Or(List(TrabajoIgualA(Mago),TrabajoIgualA(Guerrero))), modificadorIdempotente)
       espadaDelDestino.puedeEquiparse(heroe) shouldBe(false)
       espadaDelDestino.puedeEquiparse(heroe.especializarse(Ladron)) shouldBe(false)
@@ -130,7 +130,7 @@ class ProjectSpec extends AnyFreeSpec {
       espadaDelDestino.puedeEquiparse(heroe.especializarse(Guerrero)) shouldBe(true)
     }
 
-    "Item no equipable por Magos" - {
+    "Item no equipable por Magos" in {
       val pistola = UnaMano(restriccion = TrabajoDistintoDe(Mago), modificadorIdempotente)
       pistola.puedeEquiparse(heroe) shouldBe(true)
       pistola.puedeEquiparse(heroe.especializarse(Mago)) shouldBe(false)
@@ -139,34 +139,35 @@ class ProjectSpec extends AnyFreeSpec {
 
   "Items - Modificador" - {
     val heroe = Heroe(statsBase = Stats())
+    val stats = Stats().initializeWith(1)
     val cetro = UnaMano(modificador = (s:Stats, _) => s + Stats(inteligencia = 10))
     val espada = UnaMano(modificador = (s:Stats, _) => s + Stats(fuerza = 15))
     val yelmo = Torso(modificador = (s: Stats, _) => s + Stats(hp = 25))
     val sombreroDeHermes = Cabeza(modificador = (s:Stats, _) => s + Stats(velocidad = 55))
     val gemaDeLaDesolacion = Talisman(modificador = (s:Stats, _) => s + Stats().initializeWith(-50))
 
-    "Heroe con cetro deberia tener 10 de inteligencia" - {
-      heroe.equipar(cetro).stats() shouldBe Stats(inteligencia = 10)
+    "Heroe con cetro deberia tener 10 de inteligencia" in {
+      heroe.equipar(cetro).stats() shouldBe stats.copy(inteligencia = 10)
     }
 
-    "Heroe con espada deberia tener 15 de fuerza" - {
-      heroe.equipar(espada).stats() shouldBe Stats(fuerza = 15)
+    "Heroe con espada deberia tener 15 de fuerza" in {
+      heroe.equipar(espada).stats() shouldBe stats.copy(fuerza = 15)
     }
 
-    "Heroe con yelmo deberia tener 25 de hp" - {
-      heroe.equipar(yelmo).stats() shouldBe Stats(hp = 25)
+    "Heroe con yelmo deberia tener 25 de hp" in {
+      heroe.equipar(yelmo).stats() shouldBe stats.copy(hp = 25)
     }
 
-    "Heroe con el sombrero de Hermes deberia tener 55 de velocidad" - {
-      heroe.equipar(sombreroDeHermes).stats() shouldBe Stats(velocidad = 55)
+    "Heroe con el sombrero de Hermes deberia tener 55 de velocidad" in {
+      heroe.equipar(sombreroDeHermes).stats() shouldBe stats.copy(velocidad = 55)
     }
 
-    "Heroe con espada y sombrero de Hermes" - {
+    "Heroe con espada y sombrero de Hermes" in {
       heroe.equipar(sombreroDeHermes).equipar(espada).stats() shouldBe
-        Stats(velocidad = 55, fuerza = 10)
+        stats.copy(velocidad = 55, fuerza = 15)
     }
 
-    "Heroe con todos los items" - {
+    "Heroe con todos los items" in {
       heroe
         .equipar(yelmo)
         .equipar(cetro)
@@ -177,8 +178,8 @@ class ProjectSpec extends AnyFreeSpec {
 
     }
 
-    "Heroe con stats minimos" - {
-      heroe.equipar(gemaDeLaDesolacion).stats() shouldBe Stats().initializeWith(1)
+    "Heroe con stats minimos" in {
+      heroe.equipar(gemaDeLaDesolacion).stats() shouldBe stats
     }
 
   }
