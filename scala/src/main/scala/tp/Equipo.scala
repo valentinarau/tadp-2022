38bac31a357c25c325d6caa84a6ac7a51c073518
Nahuel Pascual <nahuel.pascual@mudafy.com>
2022-07-01 12:47:54 -0300

    Implementacion tareas

--- scala/src/main/scala/tp/Equipo.scala
+++ scala/src/main/scala/tp/Equipo.scala
@@ -1,8 +1,13 @@
 package tp
 
-case class Equipo(nombre: String, pozo: Int, heroe: List[Heroe]) {
-  def lider(): Option[Heroe] = ???
-  def incorporar(heroe: Heroe): Equipo = ???
+case class Equipo(nombre: String, pozo: Int, heroes: List[Heroe]) {
+  val trabajoDelLider: Option[Trabajo] = for {
+    lider <- lider()
+    trabajo <- lider.trabajo
+  } yield trabajo
+
+  def lider(): Option[Heroe] = heroes.findLast(h => 1==1) // FIXME
+  def incorporar(heroe: Heroe): Equipo = copy(heroes = heroe :: heroes)
   def obtenerItem(item: Item): Equipo = ???
   def mejorSegun(): Option[Heroe] = ??? // Falta parámetro de cuantificador
 }
--- scala/src/main/scala/tp/Tarea.scala
+++ scala/src/main/scala/tp/Tarea.scala
@@ -1,7 +1,44 @@
 package tp
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
+
+case class MisionFallidaException(tarea: Tarea) extends RuntimeException
 
 trait Tarea {
-  def facilidad(heroe: Heroe): Option[Int]
+  def facilidad(equipo: Equipo, heroe: Heroe): Option[Int]
   def intentar(heroe: Heroe): Try[Heroe]
+}
+
+case class PelearContraMonstruo(danio: Int) extends Tarea {
+  override def facilidad(equipo: Equipo, heroe: Heroe): Option[Int] = equipo.trabajoDelLider match {
+    case Some(Guerrero) => Some(20)
+    case _ => Some(10)
+  }
+
+  override def intentar(heroe: Heroe): Try[Heroe] = {
+    val heroeDaniado = if(Fuerza(heroe.stats()) < 20) heroe.modificarStats(Stats(hp = -danio)) else heroe
+    heroe match {
+      case _ if HP(heroeDaniado.statsBase) > 0 => Success(heroeDaniado)
+      case _ => Failure(MisionFallidaException(this))
+    }
+  }
+}
+
+case object ForzarPuerta extends Tarea {
+  override def facilidad(equipo: Equipo, heroe: Heroe): Option[Int] = Some(
+    Inteligencia(heroe.stats()) + 10 * equipo.heroes.count(h => h.es(Ladron))
+  )
+
+  override def intentar(heroe: Heroe): Try[Heroe] = heroe.trabajo match {
+    case Some(Ladron) | Some(Mago) => Success(heroe)
+    case _ => Success(heroe.modificarStats(Stats(hp = -5, fuerza = 1)))
+  }
+}
+
+case class RobarTalisman(talisman: Talisman) extends Tarea {
+  override def facilidad(equipo: Equipo, heroe: Heroe): Option[Int] = equipo.trabajoDelLider match {
+    case Some(Ladron) => Some(Velocidad(heroe.stats()))
+    // TODO otra clase de Hero NO PUEDE realizar la tarea
+  }
+
+  override def intentar(heroe: Heroe): Try[Heroe] = Success(heroe.equipar(talisman))
 }
