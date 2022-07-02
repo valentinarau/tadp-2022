dc6cadac29f060d40b3cf45410a959caed6b709b
Nahuel Pascual <nahuel.pascual@mudafy.com>
2022-07-02 17:43:03 -0300

    clean Equipo.scala

--- scala/src/main/scala/tp/Equipo.scala
+++ scala/src/main/scala/tp/Equipo.scala
@@ -10,28 +10,7 @@ case class Equipo(nombre: String, pozo: Int = 0, heroes: List[Heroe] = List()) {
     trabajo <- lider.trabajo
   } yield trabajo
 
-//  def lider(): Option[Heroe] = {
-//    val lead = heroes.maxByOption(h => {
-//      for {
-//        trabajo <- h.trabajo
-//      } yield trabajo.statPrincipal(h.stats())
-//    })
-//
-//    val count = heroes.map(h => {
-//      for {
-//        trabajo <- h.trabajo
-//      } yield trabajo.statPrincipal(h.stats())
-//    }).count(v => v.contains(lead.trabajo.get.statPrincipal(lead.stats())))
-//
-//    if (count== 1) {
-//      Some(lead)
-//    } else {
-//      None
-//    }
-//  }
-
   def lider(): Option[Heroe] = {
-    //val ordenados = heroes.map( h => (h, h.valorStatPrincipal())).filter( p => p._2.isDefined).sortBy(p => p._2.get)
     val ordenados = (for {
       heroe <- heroes
       valorStatPrincipal <- heroe.valorStatPrincipal()
@@ -42,19 +21,6 @@ case class Equipo(nombre: String, pozo: Int = 0, heroes: List[Heroe] = List()) {
       case (h1, _)::_ => Some(h1)
       case _ => None
     }
-//        for {
-////          statPrincipales <- heroes.map(_.valorStatPrincipal())
-//          lider <- heroes.maxByOption(h => for { statPrincipal <- h.valorStatPrincipal() } yield statPrincipal)
-//          statPrincipalLider <- lider.valorStatPrincipal()
-//        } yield lider
-//
-//    val heroesConTrabajo = heroes.filter(_.trabajo.isDefined)
-//    val statsPrincipales = heroesConTrabajo.map(_.trabajo.get.statPrincipal)
-//    if (statsPrincipales.size != statsPrincipales.distinct.size) {
-//      None
-//    } else {
-//      heroesConTrabajo.maxByOption(h => h.valorStatPrincipal())
-//    }
   }
 
   def incorporar(heroe: Heroe): Equipo = copy(heroes = heroe :: heroes)
